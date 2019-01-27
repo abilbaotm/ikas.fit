a87fb63b83cbbda8b52b5ba5c0edc636da3345d0
Asier Bilbao <abilbaotm@gmail.com>
2019-01-27 23:30:01 +0100

    permitir crear grupos

--- podiumfit/app/src/main/java/com/izabil/podiumfit/SelectorGrupo.java
+++ podiumfit/app/src/main/java/com/izabil/podiumfit/SelectorGrupo.java
@@ -1,8 +1,11 @@
 package com.izabil.podiumfit;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -14,18 +17,23 @@ import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SelectorGrupo extends AppCompatActivity {
 
@@ -118,7 +126,51 @@ public class SelectorGrupo extends AppCompatActivity {
         db.collection("users").document(mAuth.getUid()).update("grupo", db.collection("grupo").document(nuevoGrupo.getUid()));
         finish();
     }
+    private String m_Text = "";
+
     public void crearGrupo(View view){
+        //https://stackoverflow.com/a/10904665
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Crear nuevo grupo");
+
+        // Set up the input
+        final EditText input = new EditText(this);
+        // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+
+        // Set up the buttons
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Map<String, Object> nuevoGrupo = new HashMap<>();
+                nuevoGrupo.put("nombre", input.getText().toString());
+
+
+                // Add a new document with a generated ID
+                db.collection("grupo")
+                        .add(nuevoGrupo)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                //grupo creado, ahora guardar
+                                db.collection("users").document(mAuth.getUid())
+                                        .update("grupo", db.collection("grupo").document(documentReference.getId()));
+                                finish();
+                            }
+
+                        });
+
+            }
+        });
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
 
     }
 
